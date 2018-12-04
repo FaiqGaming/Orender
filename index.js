0f554adb263a0ffa6c770315e0760d9d432cb6b2
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-12-04 13:56:57 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+let prefix = 'o!';
+const fs = require("fs");
+
+client.on('ready', () => {
+  console.log('Ready A Function');
+});
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  
+  if(!message.content.startsWith(prefix));
+  let messageArray = message.content.split(" ");
+  let args = message.content.slice(prefix.length).trim().split(' ');
+  let sender = message.author;
+  let cmd = args.shift().toLowerCase()
+  
+  try {
+    let commandFile = require(`./commands/${cmd}.js`);
+    commandFile.run(client, message, args);
+  } catch (e) {
+    console.log(e.stack);
+  } finally {
+    console.log(`${message.author.tag} Running Command`);
+  }
+})
+
+client.login(process.env.BOT_TOKEN);
