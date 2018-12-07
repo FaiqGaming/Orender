e368980fa52687be9e1d4c1e718e5dc024130834
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-12-07 09:34:24 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
  let helpembed = new Discord.RichEmbed()
  .setColor('RANDOM')
  .setTitle("**OrenderClient's Command Help**")
- .addField(":hammer_pick: Utility", '**Ping,** **Say,** **BotInfo,** **ServerInfo,**')
+ .addField(":hammer_pick: Utility", '**Ping,** **Say,** **BotInfo,** **ServerInfo,** **Help,**')
  .addField(":warning: Administrator", '**Report,** **Clear,**')
  .addField(":checkered_flag: Fun", '**Cat,** **Dog,**')
  .addField("💸 Economy", '**Balance,** **Pay,** **Daily,**')
