b72dc088339dfd43f1045d706b23781e2d4c6fa3
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-12-07 09:08:21 +0900

    Update and rename ping.js to report.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = (client, message, args) => {
-  message.channel.send("Pong Current Ping Is ", client.ping)
-}
-
-module.exports.help = {
-  name: "ping"
-}
---
+++ commands/report.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+  let rUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
+  if(!rUser) return message.channel.send("Couldn't find the User!");
+  let reason = args.join(" ").slice(22);
+  
+  let reportEmbed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setTitle("Reported Logs")
+  .addField("Reported User:", `${rUser} With ID: ${rUser.id}`)
+  .addField("Reported By:", `${message.author} With ID: ${message.author.id}`)
+  .addField("Channel", message.channel)
+  .addField("Time", message.createdAt);
+  
+  let reportsChannel = message.guild.channels.find(`name`, "logs");
+  if(!reportsChannel) return message.channel.send("Couldn't find logs Channel.");
+  
+  message.delete().catch(O_o=>{});
+  reportsChannel.send(ReportEmbed);
+}
+
+module.exports.help = {
+  name: "report"
+}
