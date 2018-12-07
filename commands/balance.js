ff1d9f289c08080abc53bdb8fb104484cb89ba1a
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-12-07 09:16:19 +0900

    Delete balance.js

--- commands/balance.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-exports.run = (client, message, args) => {
-  let ertembed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .addField(":computer: Ping Is", `${client.ping}ms`)
-  
-  message.channel.send(ertembed);
-}
